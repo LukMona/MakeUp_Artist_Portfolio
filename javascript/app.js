9237eb19e2cd37ff6364e775588d5130c0ba5cce
LukMon <monikaa.lukowska@gmail.com>
2016-07-07 11:10:46 +0200

    Added form validation.

--- javascript/app.js
+++ javascript/app.js
@@ -3,7 +3,6 @@ $(window).load(function() {
   function parallaxSections(){
 
     var sections = $(".section-prlx");
-    console.log(sections);
 
     $(window).scroll(function(){
       var wScroll = $(this).scrollTop();
@@ -15,4 +14,43 @@ $(window).load(function() {
 
   parallaxSections();
 
+  function sendForm(){
+    $("#send").on("click", function(){
+      event.preventDefault();
+
+      var name = $("#name");
+      var mail = $("#mail");
+      var title = $("#title");
+      var message = $("#message");
+      var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+
+      var nameGetInfo = name.val();
+      var mailGetInfo = mail.val();
+      var messageGetInfo = message.val();
+      var titleGetInfo = title.val();
+
+      if(nameGetInfo.length < 5){
+        var newElement = $("<div class='error'>Name should be longer than 5 characters</div>");
+        newElement.insertAfter(name);
+      }
+
+      if(testEmail.test(mailGetInfo) === false ){
+        var newElement2 = $("<div class='error'>Wrong mail address</div>");
+        newElement2.insertAfter(mail);
+      }
+
+      if(messageGetInfo.length < 5){
+        var newElement3 = $("<div class='error'>Title should be longer than 5 characters</div>");
+        newElement3.insertAfter(title);
+      }
+
+      if(messageGetInfo.length < 10){
+        var newElement4 = $("<div class='error'>Message should be longer than 10 characters</div>");
+        newElement4.insertAfter(message);
+      }
+
+    });
+  }
+
+sendForm();
 });
