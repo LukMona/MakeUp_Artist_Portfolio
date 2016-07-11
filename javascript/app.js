11100a56f2a6b2617245dd569ba57eeac4be30ba
LukMon <monikaa.lukowska@gmail.com>
2016-07-11 15:03:05 +0200

    Added fullscreen images after click on images in portfolio.

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,10 @@
 $(window).load(function() {
 
   function parallaxSections(){
-
+    //looking for sections which should be on parallax
     var sections = $(".section-prlx");
 
+    //setting simple parallax
     $(window).scroll(function(){
       var wScroll = $(this).scrollTop();
       sections.css({
@@ -14,7 +15,32 @@ $(window).load(function() {
 
   parallaxSections();
 
+  function photoGallery(){
+    //looking for sections with portfolio
+    var imgs = $(".img-gallery");
+    var body = $("#portfolio");
+
+    //setting attributes for clicked imgs, full screen etc.
+    for (var i = 0; i < imgs.length; i++){
+      $(imgs[i]).on("click", function(){
+        var imgSrc = this.getAttribute("src");
+        var newDiv = $("<div class='fullScreen'><button class='close'><span>&#x2613;</span></button><img src=" + imgSrc + "></div>");
+
+        newDiv.appendTo(body);
+
+        //setting closing attribute for fullscreen img
+        newDiv.on("click", "button", function(){
+          $(".fullScreen").remove();
+        });
+
+      });
+    }
+  }
+
+  photoGallery();
+
   function sendForm(){
+    //setting attributes for sending for, to prevent from sending a form with basic errors
     $("#send").on("click", function(){
       event.preventDefault();
 
@@ -29,28 +55,32 @@ $(window).load(function() {
       var messageGetInfo = message.val();
       var titleGetInfo = title.val();
 
+      //name should has more thank 5 characters
       if(nameGetInfo.length < 5){
-        var newElement = $("<div class='alert alert-info'>Name should be longer than 5 characters</div>");
+        var newElement = $("<div>Name should be longer than 5 characters</div>");
         newElement.insertAfter(name);
       }
 
+      //mail regex
       if(testEmail.test(mailGetInfo) === false ){
-        var newElement2 = $("<div class='alert alert-info'>Wrong mail address</div>");
+        var newElement2 = $("<div'>Wrong mail address</div>");
         newElement2.insertAfter(mail);
       }
 
+      //message title should has more thank 5 characters
       if(messageGetInfo.length < 5){
-        var newElement3 = $("<div class='alert alert-info'>Title should be longer than 5 characters</div>");
+        var newElement3 = $("<div>Title should be longer than 5 characters</div>");
         newElement3.insertAfter(title);
       }
 
+      //message title should has more thank 10 characters
       if(messageGetInfo.length < 10){
-        var newElement4 = $("<div class='alert alert-info'>Message should be longer than 10 characters</div>");
+        var newElement4 = $("<div>Message should be longer than 10 characters</div>");
         newElement4.insertAfter(message);
       }
 
     });
   }
 
-sendForm();
+  sendForm();
 });
